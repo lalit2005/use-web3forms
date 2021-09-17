4ef1b3c9ea136c18ab8362142f9b45e930af6456
Lalit <lalitvijay9480@gmail.com>
2021-09-17 14:49:02 +0530

    feat: finish basic functionality

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React from "react";
 
-import { useMyHook } from 'use-web3forms'
+import useWeb3forms from "use-web3forms";
 
 const App = () => {
-  const example = useMyHook()
+  const { submit } = useWeb3forms("0e83ba98-55ad-4b87-8684-4175fd6e2ab6");
   return (
     <div>
-      {example}
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          submit({
+            hello: "world",
+            sentOn: "Clicking a button",
+          });
+        }}>
+        Send mail
+      </button>
+      {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
     </div>
-  )
-}
-export default App
+  );
+};
+export default App;
