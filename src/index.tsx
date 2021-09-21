ac23bbcc63b731c8bcb84db604b5f5575b5d7f65
Lalit <lalitvijay9480@gmail.com>
2021-09-21 18:03:22 +0530

    feat: add prettier

--- src/index.tsx
+++ src/index.tsx
@@ -8,10 +8,10 @@ const useWeb3forms = <T extends {}>({
   onError: (errorMessage: string, data: Response<T>) => void;
 }) => {
   const submit = async (formData: T) => {
-    const response = await fetch("https://api.web3forms.com/submit", {
-      method: "POST",
+    const response = await fetch('https://api.web3forms.com/submit', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
         apikey,
